0488f1e49f3abdabf7b3bd484300802d621fbfc0
Gabor A <galbitz@gmail.com>
2023-01-06 17:46:26 -0500

    Sentry integratio

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,99 +1,99 @@
+import * as Sentry from "@sentry/browser";
 import { auth, db } from "../lib/firebase";
 import { getBrowserId, openManager, TAB_MANAGER_COMMAND } from "../lib/browser";
 import browser from "webextension-polyfill";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { sentryConfig } from "../lib/sentry";
 
 export {};
 
-try {
-  browser.runtime.onInstalled.addListener((details) => {
-    console.log("[background.js] onInstalled", details);
-    saveTabs();
-    saveBrowserInfo();
-  });
+Sentry.init(sentryConfig);
 
-  browser.runtime.onStartup.addListener(() => {
-    console.log("[background.js] onStartup");
-    saveTabs();
-    saveBrowserInfo();
-  });
+browser.runtime.onInstalled.addListener((details) => {
+  console.log("[background.js] onInstalled", details);
+  saveTabs();
+  saveBrowserInfo();
+});
 
-  browser.tabs.onRemoved.addListener(
-    async (tabId: number, removeInfo: object) => {
-      console.log("removed", tabId, removeInfo);
+browser.runtime.onStartup.addListener(() => {
+  console.log("[background.js] onStartup");
+  saveTabs();
+  saveBrowserInfo();
+});
+
+browser.tabs.onRemoved.addListener(
+  async (tabId: number, removeInfo: object) => {
+    console.log("removed", tabId, removeInfo);
+    saveTabs();
+  }
+);
+
+browser.tabs.onUpdated.addListener(
+  async (tabId: number, changeInfo, tab: browser.Tabs.Tab) => {
+    if (
+      changeInfo.status === "complete" ||
+      (!changeInfo.status && (!changeInfo.title || !changeInfo.url))
+    ) {
+      console.log("Tab loaded: ", tab.url);
       saveTabs();
     }
-  );
+  }
+);
 
-  browser.tabs.onUpdated.addListener(
-    async (tabId: number, changeInfo, tab: browser.Tabs.Tab) => {
-      if (
-        changeInfo.status === "complete" ||
-        (!changeInfo.status && (!changeInfo.title || !changeInfo.url))
-      ) {
-        console.log("Tab loaded: ", tab.url);
-        saveTabs();
-      }
-    }
-  );
+const saveData = async (userId: string, browserId: string, data: any) => {
+  //TODO: move this to firebase lib
+  const documentPath = `users/${userId}/browsers/${browserId}`;
+  const docRef = doc(db, documentPath);
+  try {
+    await setDoc(
+      docRef,
+      { ...data, updatedAt: serverTimestamp() },
+      { merge: true }
+    );
+  } catch (exception) {
+    console.log("[background.js] saveData ", exception);
+  }
+};
 
-  const saveData = async (userId: string, browserId: string, data: any) => {
-    //TODO: move this to firebase lib
-    const documentPath = `users/${userId}/browsers/${browserId}`;
-    const docRef = doc(db, documentPath);
-    try {
-      await setDoc(
-        docRef,
-        { ...data, updatedAt: serverTimestamp() },
-        { merge: true }
-      );
-    } catch (exception) {
-      console.log("[background.js] saveData ", exception);
-    }
-  };
+const saveTabs = async () => {
+  console.log("[background.js] savetabs");
 
-  const saveTabs = async () => {
-    console.log("[background.js] savetabs");
+  const browserInfo = await browser.windows.getAll({ populate: true });
 
-    const browserInfo = await browser.windows.getAll({ populate: true });
+  if (!auth.currentUser || !auth.currentUser.emailVerified || !browserInfo)
+    return;
 
-    if (!auth.currentUser || !auth.currentUser.emailVerified || !browserInfo)
-      return;
+  //await browser.storage.local.set({ browser: browserInfo });
+  const currentBrowser = await getBrowserId();
 
-    //await browser.storage.local.set({ browser: browserInfo });
-    const currentBrowser = await getBrowserId();
-
-    await saveData(auth.currentUser.uid, currentBrowser, {
-      windows: browserInfo,
-    });
-  };
-
-  const saveBrowserInfo = async () => {
-    if (!auth.currentUser || !auth.currentUser.emailVerified) return;
-
-    const currentBrowser = await getBrowserId();
-    const platformInfo = await browser.runtime.getPlatformInfo();
-
-    saveData(auth.currentUser.uid, currentBrowser, {
-      platformInfo: platformInfo,
-    });
-  };
-
-  auth.onAuthStateChanged(async (user) => {
-    if (user) {
-      await saveTabs();
-      await saveBrowserInfo();
-    }
+  await saveData(auth.currentUser.uid, currentBrowser, {
+    windows: browserInfo,
   });
+};
 
-  browser.commands.onCommand.addListener(async (command) => {
-    console.log(`Command: ${command}`);
-    if (command !== TAB_MANAGER_COMMAND) {
-      return;
-    }
+const saveBrowserInfo = async () => {
+  if (!auth.currentUser || !auth.currentUser.emailVerified) return;
 
-    openManager();
+  const currentBrowser = await getBrowserId();
+  const platformInfo = await browser.runtime.getPlatformInfo();
+
+  saveData(auth.currentUser.uid, currentBrowser, {
+    platformInfo: platformInfo,
   });
-} catch (exception) {
-  console.log("[background.js] ", exception);
-}
+};
+
+auth.onAuthStateChanged(async (user) => {
+  if (user) {
+    await saveTabs();
+    await saveBrowserInfo();
+  }
+});
+
+browser.commands.onCommand.addListener(async (command) => {
+  console.log(`Command: ${command}`);
+  if (command !== TAB_MANAGER_COMMAND) {
+    return;
+  }
+
+  openManager();
+});
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Popup from "./Popup";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+import { sentryConfig } from "../lib/sentry";
+
+Sentry.init(sentryConfig);
 
 ReactDOM.render(
   <React.StrictMode>
